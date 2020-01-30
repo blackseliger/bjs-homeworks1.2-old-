1df5f391b5fef0a76ad57e3f6a8d1aec4e190661
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-01-30 23:23:27 +0300

    Сделнная первая работа 23:23 30 янв

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -11,6 +11,32 @@ function calculateQuadraticEquation(){
 }
 
 function getResult(a,b,c){
+    'use strict';
+let a;
+let b;
+let c;
+// d = b**2 - 4*a*c 
+let resultofD = [];
+
+Math.pow(b, 2);
+let d = b - 4*a*c;
+if ( d < 0) {
+    resultofD = [];
+} else if ( d > 0 ) {
+    let x1, x2;
+    Math.sqrt(d);
+    x1 = ( (-b) + d )/(2*a);
+    x2 = ( (b) + d )/(2*a);
+    resultofD.push(x1 , x2);
+    console.log(resultofD); 
+} else if ( d = 0) {
+    let x1;
+    Math.sqrt(d);
+    x1 = ( (-b) + d )/(2*a);
+    resultofD.push(x1);
+    console.log(resultofD);
+}
+return x1, x2;
     // код для задачи №1 писать здесь
     //return x;
 }
