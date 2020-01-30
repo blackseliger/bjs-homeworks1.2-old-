9d7451d6d64fecc6199dff7eee08bd008f5caeef
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-01-31 00:04:32 +0300

    Сделан. вторая работа

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -12,9 +12,9 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     'use strict';
-let a;
-let b;
-let c;
+// let a;
+// let b;
+// let c;      Что значит реализовать функцию? Такой же вопрос на 51 строке.
 // d = b**2 - 4*a*c 
 let resultofD = [];
 
@@ -36,18 +36,32 @@ if ( d < 0) {
     resultofD.push(x1);
     console.log(resultofD);
 }
-return x1, x2;
+// return x1, x2;
     // код для задачи №1 писать здесь
     //return x;
 }
 
-function calculateAverageRating(){
+function calculateAverageRating() {
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
 }
 
 function getAverageMark(marks){
+    let intialMarks = [];
+    let sum;
+    intialMarks.push(marks)
+    if ( intialMarks.length > 5) {
+        intialMarks.slice(0, 5);
+        console.log('Превышен лимит введенных оценок');
+    } else if (intialMarks.length <=5) {
+        for (let i; i < intialMarks; i++) {
+      
+        sum =+ intialMarks;
+        }
+        averageMark = sum/intialMarks.length;
+        console.log(averageMark);
+    }
     // код для задачи №2 писать здесь
     //return averageMark;
 }
